c51483576cd730728b0e2f16c1df859628b08102
Korte Maki <kortemaki@gmail.com>
2025-09-17 23:31:00 -0400

    Implement Linear

--- minitorch/modules_basic.py
+++ minitorch/modules_basic.py
@@ -16,6 +16,11 @@ from .tensor import Tensor
 from typing import Any, Dict, Optional, Sequence, Tuple
 
 
+def UniformRandomParameter(w, *shape):
+    r = (2*w)*(minitorch.rand(shape, backend=BACKEND) - w)
+    return minitorch.Parameter(r)
+
+
 class Embedding(Module):
     def __init__(self, num_embeddings: int, embedding_dim: int, backend: TensorBackend):
         super().__init__()
@@ -35,7 +40,7 @@ class Embedding(Module):
         ### BEGIN ASSIGN3_2
         raise NotImplementedError
         ### END ASSIGN3_2
-    
+
     def forward(self, x: Tensor):
         """Maps word indices to one-hot vectors, and projects to embedding vectors.
 
@@ -50,24 +55,24 @@ class Embedding(Module):
         raise NotImplementedError
         ### END ASSIGN3_2
 
-    
+
 class Dropout(Module):
     def __init__(self, p_dropout: float=0.1):
         super().__init__()
         """During training, randomly zeroes some of the elements of the input tensor with probability :attr:`p_dropout`.
 
-        Attributes: 
+        Attributes:
             p_dropout : Probability an element will be zeroed.
         """
         self.p_dropout = p_dropout
 
-    def forward(self, x: Tensor) -> Tensor: 
+    def forward(self, x: Tensor) -> Tensor:
         """During training, randomly zero out elements of a tensor and scale by (1 - p_dropout)
-        
-        Args: 
+
+        Args:
             x : Tensor of shape (*)
-        
-        Returns: 
+
+        Returns:
             output : Tensor of shape (*)
         """
         ### BEGIN ASSIGN3_2
@@ -91,21 +96,27 @@ class Linear(Module):
         """
         self.out_size = out_size
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        w = 1/sqrt(in_size)
+        self._parameters['weights'] = self.weights = UniformRandomParameter(backend, w, in_size, out_size)
+        self.bias = UniformRandomParameter(backend, w, out_size) if bias else Tensor(backend=backend).zeros(out_size)
+        if bias:
+            self._parameters['bias'] = self.bias
         ### END ASSIGN3_2
 
     def forward(self, x: Tensor):
         """Applies a linear transformation to the incoming data.
-        
-        Args: 
+
+        Args:
             x : Tensor of shape (n, in_size)
-        
+
         Returns:
             output : Tensor of shape (n, out_size)
         """
         batch, in_size = x.shape
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        return (
+            x.view(batch, in_size) @ self.weights.value.view(in_size, self.out_size)
+        ).view(batch, self.out_size) + self.bias.value
         ### END ASSIGN3_2
 
 
@@ -113,12 +124,12 @@ class LayerNorm1d(Module):
     def __init__(self, dim: int, eps: float, backend: TensorBackend):
         super().__init__()
         """Applies Layer Normalization over a mini-batch of 1-dimensional inputs.
-        
-        Args: 
+
+        Args:
             dim : Expected size of the last dimension to apply layer normalization.
             eps : A value added for numerical stability.
-        
-        Attributes: 
+
+        Attributes:
             weights : the learnable weights of the module of shape (self.dim, ) initialized to 1.
             bias    : the learnable bias of the module of shape (self.dim, ) initialized to 0.
         """
@@ -129,14 +140,14 @@ class LayerNorm1d(Module):
         ### END ASSIGN3_2
 
     def forward(self, x: Tensor) -> Tensor:
-        """Applies Layer Normalization over a mini-batch of inputs. 
+        """Applies Layer Normalization over a mini-batch of inputs.
         NOTE: You can assume the input to this layer is a 2D tensor of shape (batch_size, dim)
         You will use implicit broadcasting in miniTorch to use the weight and bias.
-        
-        Input: 
+
+        Input:
             x - Tensor of shape (bs, dim)
-        
-        Output: 
+
+        Output:
             output - Tensor of shape (bs, dim)
         """
         batch, dim = x.shape
