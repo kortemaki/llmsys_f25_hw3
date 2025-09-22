66ff8960b15bfc80dcae3a6eaabe8747eddee99c
Korte Maki <kortemaki@gmail.com>
2025-09-22 02:54:47 +0000

    Fix dropout layer tensor initialization

--- minitorch/modules_basic.py
+++ minitorch/modules_basic.py
@@ -83,7 +83,10 @@ class Dropout(Module):
             return x
         if self.p_dropout >= 1:
             return x.zeros()
-        return Mul.apply(x * 1/(1 - self.p_dropout), np.random.binomial(1, 1 - self.p_dropout, x.shape))
+        return Mul.apply(
+            x * 1/(1 - self.p_dropout),
+            tensor_from_numpy(np.random.binomial(1, 1 - self.p_dropout, x.shape), backend=x.backend)
+        )
         ### END ASSIGN3_2
 
 
