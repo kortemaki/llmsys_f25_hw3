90b098e3086f724c4095be2c4258d433a9a49dca
Korte Maki <kortemaki@gmail.com>
2025-09-23 10:56:36 -0400

    cleanup

--- minitorch/modules_transfomer.py
+++ minitorch/modules_transfomer.py
@@ -89,19 +89,16 @@ class MultiHeadAttention(Module):
         ### BEGIN ASSIGN3_3
         q = (
             self.q_projection(x.view(batch_size * seq_len, n_embd))
-            #.view(batch_size, seq_len, n_embd)
             .view(batch_size, seq_len, self.n_head, self.attn_hidden_dim)
             .permute(0, 2, 1, 3)
         )
         kT = (
             self.k_projection(x.view(batch_size * seq_len, n_embd))
-            #.view(batch_size, seq_len, n_embd)
             .view(batch_size, seq_len, self.n_head, self.attn_hidden_dim)
             .permute(0, 2, 3, 1)
         )
         v = (
             self.v_projection(x.view(batch_size * seq_len, n_embd))
-            #.view(batch_size, seq_len, n_embd)
             .view(batch_size, seq_len, self.n_head, self.attn_hidden_dim)
             .permute(0, 2, 1, 3)
         )
